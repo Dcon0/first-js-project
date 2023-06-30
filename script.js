56b01f960f8105979cfe7896b4399364fa8a6bfb
Yassine Slaoui <yassine_slaoui51@outlook.fr>
2023-06-30 08:28:57 +0100

    Separated JS from HTML file

---
+++ script.js
@@ -0,0 +1,37 @@
+let todoIdArr = [];
+function addTodo() {
+    let textBox = document.getElementById("todoStr");
+    if (textBox.value == "") return;
+    let element = document.createElement("div");
+    let id = "todoItemNo" + todoIdArr.length;
+    element.setAttribute("id", id);
+    todoIdArr.push(id);
+    // element.innerText = textBox.value;
+    element.innerHTML =
+        textBox.value +
+        "<button onclick= \'document.getElementById('" + id + "').remove();todoArr.splice(+todoArr.indexOf('" + id + "'),1);\">Remove this Item</button>";
+    document.body.appendChild(element);
+    textBox.value = "";
+}
+function resetToDoList() {
+    for (let i = todoIdArr.length - 1; i >= 0; i--)
+        document.getElementById(todoIdArr[i]).remove();
+    todoIdArr = [];
+}
+function getTodoArr() {
+    let todoArr = [];
+    todoIdArr.forEach((e) => {
+        let todoItem = document.getElementById(e).innerText;
+        todoArr.push(todoItem.substring(0, todoItem.length - 16));
+    });
+    return todoArr;
+}
+document
+    .getElementById("addTodoBtn")
+    .setAttribute("onclick", "addTodo()");
+document
+    .getElementById("todoStr")
+    .setAttribute("onkeypress", "if(event.key == 'Enter') addTodo()");
+document
+    .getElementById("resetTodoBtn")
+    .setAttribute("onclick", "resetToDoList()");
