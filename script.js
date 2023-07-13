649e8db2a729693367adaf2a1a71f1ef23265c6a
Yassine Slaoui <yassine_slaoui51@outlook.fr>
2023-07-13 21:38:59 +0100

    Some new things

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 let todoIdArr = [];
+const todoListDiv = document.getElementById("todoList");
+const textBox = document.getElementById("todoStr");
+let counter = 0
+
+function removeTodo(event) {
+    const deleteButton = event.target;
+    document.getElementById(deleteButton.getAttribute("parentdivid")).remove();
+    // todoIdArr.splice(todoIdArr.indexOf(deleteButton.getAttribute("parentdivid")), 1);
+    todoIdArr = todoIdArr.filter(todoItem => {
+        if (todoItem === deleteButton.getAttribute("parentdivid"))
+            return false;
+        return true;
+    })
+}
+
 function addTodo() {
-    let textBox = document.getElementById("todoStr");
     if (textBox.value == "") return;
-    let element = document.createElement("div");
-    let id = "todoItemNo" + todoIdArr.length;
-    element.setAttribute("id", id);
+    const newTodoItem = document.createElement("div");
+    const id = "todoItemNo" + (counter++);
+    newTodoItem.id = id;
     todoIdArr.push(id);
-    // element.innerText = textBox.value;
-    element.innerHTML =
-        textBox.value +
-        "<button onclick= \'document.getElementById(\"" + id + "\").remove();todoIdArr.splice(todoIdArr.indexOf(\"" + id + "\"),1);\'>Remove this Item</button>";
-    document.body.appendChild(element);
+    newTodoItem.innerText = textBox.value;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Remove this Item"
+    deleteButton.setAttribute("parentdivid", id);
+    deleteButton.onclick = removeTodo;
+    newTodoItem.appendChild(deleteButton);
+    todoListDiv.appendChild(newTodoItem);
     textBox.value = "";
 }
 function resetToDoList() {
-    for (let i = todoIdArr.length - 1; i >= 0; i--)
-        document.getElementById(todoIdArr[i]).remove();
+    todoListDiv.innerHTML = "";
     todoIdArr = [];
 }
 function getTodoArr() {
